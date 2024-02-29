8129a01ce98f18b6b4640756986bfa9a3cf71ebd
Arnab <iamarnabbiswas2001@gmail.com>
2024-02-29 13:32:11 +0530

    drawer

--- src/components/common/Header2.jsx
+++ src/components/common/Header2.jsx
@@ -1,9 +1,58 @@
 import { Box, Image, Button, UnorderedList } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 import { useMediaQuery } from "@chakra-ui/react";
+import { useRef } from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Input
+} from '@chakra-ui/react'
+import { useDisclosure } from "@chakra-ui/react";
+
+
+
+
+
+
 export default function Header2() {
   const [isLargerThan980] = useMediaQuery("(min-width: 980px)");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef =useRef()
   return (
+    <>
+     <Drawer
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader> <Image
+        src="./assets/gd logo.gif"
+        height={"80%"}
+        width={"60%"}
+        m={2}
+        cursor={"pointer"}
+      ></Image></DrawerHeader>
+
+          <DrawerBody>
+          <UnorderedList fontFamily="Georgia, serif"  display={"flex"} flexDirection={'column'} gap={4} color={'black'}>
+          <NavLink to="#">Home</NavLink>
+          <NavLink to="#">About Us</NavLink>
+          <NavLink to="#">Packages</NavLink>
+          <NavLink to="#">Dates</NavLink>
+        </UnorderedList>
+          </DrawerBody>
+
+
+        </DrawerContent>
+      </Drawer>
     <Box
       minW={"100%"}
       display={"flex"}
@@ -42,8 +91,11 @@ export default function Header2() {
           height={"60px"}
           m={2}
           cursor={"pointer"}
+          ref={btnRef}
+          onClick={onOpen}
         ></Image>
       )}
     </Box>
+    </>
   );
 }
